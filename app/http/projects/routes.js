8a64f186b011925049d4d30f18ab28105df2589c
Muhammad Ilyas <ilyas@zaffretech.co>
2024-07-15 23:31:33 +0500

    create

--- app/http/projects/routes.js
+++ app/http/projects/routes.js
@@ -5,7 +5,7 @@ const validations = require('./validations')
 const { verifyToken } = require('../../middlewares')
 
 router.get('/list', [verifyToken], controller.list);
-router.post('/create', [verifyToken, validations.create], controller.list);
+router.post('/create', [verifyToken, validations.create], controller.create);
 router.patch('/update/:id', [verifyToken, validations.update], controller.update);
 router.delete('/delete/:id', [verifyToken], controller.delete);
 
