b4aff36879117d73f1ed7a4cc5aefae487e4869c
Mohammad Waqar <waqar@zaffretech.co>
2024-09-19 23:34:33 +0500

    Update routes.js

--- app/http/projects/routes.js
+++ app/http/projects/routes.js
@@ -4,13 +4,13 @@ const controller = require('./controller')
 const validations = require('./validations')
 const { verifyToken } = require('../../middlewares');
 
-router.get('/list', [verifyToken], controller.list);
-router.get('/details/:id', [verifyToken], controller.details);
-router.post('/create', [verifyToken  ,validations.create ], controller.create);
-router.patch('/update/:id', [verifyToken , validations.update], controller.update);
-router.delete('/delete/:id', [verifyToken], controller.delete);
+router.get('/list', [verifyToken], (req, res) => controller.list(req, res));
+router.get('/details/:id', [verifyToken], (req, res) => controller.details(req, res));
+router.post('/create', [verifyToken, validations.create], (req, res) => controller.create(req, res));
+router.patch('/update/:id', [verifyToken, validations.update], (req, res) => controller.update(req, res));
+router.delete('/delete/:id', [verifyToken], (req, res) => controller.delete(req, res));
 
-router.post('/attachments/create', [verifyToken , validations.createAttachment], controller.createAttachment);
-router.delete('/attachments/delete/:id', [verifyToken], controller.deleteAttachment);
+router.post('/attachments/create', [verifyToken, validations.createAttachment], (req, res) => controller.createAttachment(req, res));
+router.delete('/attachments/delete/:id', [verifyToken], (req, res) => controller.deleteAttachment(req, res));
 
 module.exports = router;
