7a4c5ffff9215f3c74e10c4a7fa42f5fbb3c0e1e
Mohammad Waqar <waqar@zaffretech.co>
2024-09-30 19:37:43 +0500

    Update app.js

--- bin/app.js
+++ bin/app.js
@@ -23,17 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '.../', 'public')));
 
-app.use(cors({
-  origin: function (origin, callback) {
-    callback(null, true);
-    // if (!origin || whitelist.indexOf(origin) !== -1) {
-    //   callback(null, true);
-    // } else {
-    //   callback(new Error("Not allowed by CORS"));
-    // }
-  },
-  credentials: true,
-}));
+app.use(cors());
 
 app.use((req, res, next) => {
   res.language = req.headers["Accept-Language"] || "en"
