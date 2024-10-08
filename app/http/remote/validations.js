f4a0fc83d2d94b5273691cc3a966f629b64eca51
Mohammad Waqar <waqar@zaffretech.co>
2024-10-08 19:31:55 +0500

    Update validations.js

--- app/http/remote/validations.js
+++ app/http/remote/validations.js
@@ -19,7 +19,9 @@ module.exports = {
   processStats: async (req, res, next) => {
     try {
       await Joi.object({
-
+        process: Joi.array().required().messages({
+          'any.required': "proccessRequired",
+        }),
       }).validateAsync(req.body);
       next();
     } catch (error) {
@@ -29,7 +31,9 @@ module.exports = {
   screenshots: async (req, res, next) => {
     try {
       await Joi.object({
-
+        urls: Joi.array().required().messages({
+          'any.required': "urlsRequired",
+        }),
       }).validateAsync(req.body);
       next();
     } catch (error) {
